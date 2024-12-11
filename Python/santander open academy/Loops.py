a18228742c1c01973d4671f9e6137c698ed8497f
Ketlin Casagrande <Ketlin.casagrande010@gmail.com>
2024-12-10 22:45:25 -0300

    Upload

---
+++ Python/santander open academy/Loops.py
@@ -0,0 +1,10 @@
+print('Números de 1 a 5 multiplicados por 2 com loop for:')
+for numero in range(1, 6):
+    print(numero * 2)
+
+print('\nNúmeros de 1 a 5 multiplicados por 2 com loop while:')
+contador = 1
+while contador <= 5:
+    print(contador * 2)
+    contador +=1
+    
