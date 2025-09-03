998f53d53eaacc36bbd74992afb8da22e3ed83ac
Ketlin Casagrande <158323965+KetlinCasagrande@users.noreply.github.com>
2025-09-02 22:43:13 -0300

    Upload

---
+++ Python/Faculdade/Conceitos.py
@@ -0,0 +1,21 @@
+print("Hello Word!")
+print ("POO")
+
+
+#string
+nome = "Ketinha"
+
+#inteiro
+
+idade = 20
+
+
+
+
+
+
+print(idade)
+
+print(nome)
+nomedigitado = input ("Por Favor Digite um Nome:")
+print ( " O nome digitado foi: ", nomedigitado) 
